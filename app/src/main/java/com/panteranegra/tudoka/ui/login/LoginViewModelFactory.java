e91e6a9f3adf8a224b21ee07b688d917fee202a8
lulea <oscar@ernieplus.com>
2019-10-09 23:06:59 -0500

    Agregar soporte a Kotlin, migrar a AndroidX

--- app/src/main/java/com/panteranegra/tudoka/ui/login/LoginViewModelFactory.java
+++ app/src/main/java/com/panteranegra/tudoka/ui/login/LoginViewModelFactory.java
@@ -1,8 +1,8 @@
 package com.panteranegra.tudoka.ui.login;
 
-import android.arch.lifecycle.ViewModel;
-import android.arch.lifecycle.ViewModelProvider;
-import android.support.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
 
 import com.panteranegra.tudoka.data.LoginDataSource;
 import com.panteranegra.tudoka.data.LoginRepository;
--- app/src/main/java/com/panteranegra/tudoka/ui/login/Prueba.java
+++ app/src/main/java/com/panteranegra/tudoka/ui/login/Prueba.java
@@ -1,8 +1,8 @@
 package com.panteranegra.tudoka.ui.login;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.panteranegra.tudoka.R;
 
